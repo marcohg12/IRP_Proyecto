d820c720ffb86d096058d2467430bbdff0cf1724
Marco Herrera <marcohg0312@gmail.com>
2024-06-01 11:06:01 -0600

    Function prototypes

--- capturer.py
+++ capturer.py
@@ -1,3 +1,9 @@
 # ---------------------------------------------------------------------------------------------------
 # Este archivo contiene el módulo del capturador de imágenes y video del sistema
 # ---------------------------------------------------------------------------------------------------
+
+def process_video():
+    pass
+
+def process_image():
+    pass
--- report.py
+++ report.py
@@ -1,3 +1,16 @@
 # ---------------------------------------------------------------------------------------------------
 # Este archivo contiene el módulo del generador de reporte del sistema
 # ---------------------------------------------------------------------------------------------------
+
+SESSION_FILENAME = None
+TIME_STAMP = None
+LAST_NUMBER_OF_MACAWS_SEEN = 0
+
+def process_time_stamp():
+    pass
+
+def report_director():
+    pass
+
+def write_in_file():
+    pass
