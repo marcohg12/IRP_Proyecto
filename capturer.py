2b1c19512431c1a5aafad1e7dbf85ba9a1201d18
DaniAguilar22 <79284307+DaniAguilar22@users.noreply.github.com>
2024-06-01 15:29:06 -0600

    classifier y video_process

--- capturer.py
+++ capturer.py
@@ -1,9 +1,45 @@
 # ---------------------------------------------------------------------------------------------------
 # Este archivo contiene el módulo del capturador de imágenes y video del sistema
 # ---------------------------------------------------------------------------------------------------
+import cv2 
 
-def process_video():
+def process_video(video_path):
+    video_capture = cv2.VideoCapture(video_path)
+    
+    frame_count = 0
+    timestamps = []
+    frames = []
+    
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
+    frame_interval = int(fps) 
+    
+    while video_capture.isOpened():
+        ret, frame = video_capture.read()
+        
+        if ret and frame_count % frame_interval == 0:
+            frames.append(frame)
+            timestamps.append(video_capture.get(cv2.CAP_PROP_POS_MSEC))
+            
+        frame_count += 1
+        
+        if not ret:
+            break
+    
+    video_capture.release()
+    cv2.destroyAllWindows()
+    
+    # Convert timestamps from milliseconds to minutes and seconds
+    timestamps_min_sec = [(int(timestamp) // 60000, int((timestamp % 60000) / 1000)) for timestamp in timestamps]
+    
+    return frames, timestamps_min_sec
+
+def process_image(image, timestamp):
+
     pass
 
-def process_image():
-    pass
+# Example usage
+video_path = "videos/video_1.mp4"
+frames, timestamps = process_video(video_path)
+
+for i, (frame, (minutes, seconds)) in enumerate(zip(frames, timestamps)):
+    print(f"Frame {i} saved at timestamp {minutes} minutes and {seconds} seconds.")
--- classifier.py
+++ classifier.py
@@ -1,7 +1,39 @@
 # ---------------------------------------------------------------------------------------------------
 # Este archivo contiene el módulo del clasificador del sistema
 # ---------------------------------------------------------------------------------------------------
+from ultralytics import YOLO 
+import os
+
+#Function to detect macaws in an image
+def detect_macaws(image_path):
+    model = YOLO("Train4.0/runs/detect/train/weights/best.pt", task="detect")  
+
+    results = model(image_path)
+
+    num_macaws = 0
+    
+    for result in results:
+        boxes = result.boxes
+        
+        num_macaws += len(boxes)
+        print(result)
+    
+    print(f"Number of macaws detected: {num_macaws}")
+    
+    return num_macaws
+
+#Function to go trough all the images in a folder
+def go_through_images(folder_path):
+    
+    files = os.listdir(folder_path)
+
+    for file_name in files:
+        file_path = os.path.join(folder_path, file_name)
+        detect_macaws(file_path)
 
 
-def detect_macaws():
-    pass
+def main():
+    go_through_images("images_filter")
+
+if __name__ == "__main__":
+    main()
